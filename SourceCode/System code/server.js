374329c89314fa8f4aae1b1e75406ca7efae9c60
AlistairJosling24 <alistairjosling@outlook.com>
2026-03-01 15:48:20 +0000

    Add files via upload
    final version

--- SourceCode/System code/server.js
+++ SourceCode/System code/server.js
@@ -17,3 +17,9 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
+
+const authMiddleware = require("./middleware/authMiddleware");
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({ message: "You accessed a protected route!" });
+});
